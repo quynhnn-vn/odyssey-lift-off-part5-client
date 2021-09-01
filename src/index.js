6a2fb9d87038e329343e2c2313f5fff09216c6f1
quynhnn-vn <nnquynh.0111@gmail.com>
2021-09-01 13:28:43 +0200

    Update uri in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://catstronauts-qnn.herokuapp.com/',
+  uri: 'https://server-catstronauts-qnn.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
